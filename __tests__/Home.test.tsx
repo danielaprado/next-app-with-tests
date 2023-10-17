bb1cbe1b1c7d954d48b8965331f5eda9034693c6
Daniela Prado <danielahortaprado@gmail.com>
2023-10-17 21:42:17 +0100

    fix: error when running test cmd

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,6 +1,5 @@
 import Home from "@/app/page";
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
 
 it("should have Docs text", () => {
   render(<Home />); //Arrange the test
